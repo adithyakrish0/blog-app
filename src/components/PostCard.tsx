e6c142853f0249160a1d06fd114dec355b0b1aa5
Adithyakrishnan P N <adiedu5204@gmail.com>
2025-08-10 17:45:24 +0530

    Update PostCard.tsx

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,7 +4,7 @@ import { Post } from '@/lib/api';
 export default function PostCard({ post }: { post: Post }) {
   return (
     <article className="post-card">
-      {/* First Link - Fixed to use string literal */}
+      {/* First Link */}
       <Link href={`/posts/${post.id}`}>
         <div className="post-image" style={{ height: '180px' }}>
           <img 
@@ -20,7 +20,7 @@ export default function PostCard({ post }: { post: Post }) {
       </Link>
       
       <div className="post-content">
-        {/* Second Link - Fixed the syntax error and RegExp issue */}
+        {/* Second Link - Fixed the RegExp issue */}
         <Link href={`/posts/${post.id}`}>
           <h3 className="post-title text-underline">{post.title}</h3>
         </Link>
@@ -30,7 +30,6 @@ export default function PostCard({ post }: { post: Post }) {
         <div className="post-meta">
           <div className="post-avatar">
             <img 
-              // Fixed the image source syntax
               src={`https://picsum.photos/seed/${post.author}/100/100.jpg`}
               alt={post.author}
               style={{ 
@@ -42,7 +41,7 @@ export default function PostCard({ post }: { post: Post }) {
             />
           </div>
           <span className="post-author">{post.author}</span>
-          <span>•</span> {/* Added bullet separator */}
+          <span>•</span>
           <span>{post.readTime} min read</span>
         </div>
       </div>
